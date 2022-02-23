12aff9ddd488745dd26abe151f8ed56b3240314b
7Teen17 <7Teen17.email@gmail.com>
2022-02-22 22:24:20 -0800

    idk what im doing

--- src/main/java/com/github/seventeen/automessage/AutoMessage.java
+++ src/main/java/com/github/seventeen/automessage/AutoMessage.java
@@ -3,6 +3,7 @@ package com.github.seventeen.automessage;
 import net.fabricmc.api.ModInitializer;
 
 public class AutoMessage implements ModInitializer {
+    public static final String MOD_ID = "automessage";
     @Override
     public void onInitialize() {
 
---
+++ src/main/java/com/github/seventeen/automessage/mixin/MinecraftMixin.java
@@ -0,0 +1,8 @@
+package com.github.seventeen.automessage.mixin;
+
+import net.minecraft.entity.Entity;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(Entity.class)
+public class MinecraftMixin {
+}
