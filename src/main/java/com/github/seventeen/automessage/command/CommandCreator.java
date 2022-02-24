bdb4ba64be7e8bb7059fcfe3b9cbc78c95f45a41
Seventeen <90122630+7Teen17@users.noreply.github.com>
2022-02-24 08:16:23 -0800

    added arguments and made minor tweaks

--- src/main/java/com/github/seventeen/automessage/command/CommandCreator.java
+++ src/main/java/com/github/seventeen/automessage/command/CommandCreator.java
@@ -13,7 +13,11 @@ import java.util.Map;
 
 public class CommandCreator {
     private Map<Integer, List> messages = new ArrayList<>();
-
+    
+    //maps the user inputted string id to the synctask int id
+    //idConversion.add(id, syncdelayedtask())
+    private Map<String, Integer> idConversion = new ArrayList<>();
+        
     public static void createCommands() {
         ClientCommandManager.DISPATCHER.register(
                 ClientCommandManager.literal("automessage").executes(context -> {
@@ -26,12 +30,14 @@ public class CommandCreator {
                     MinecraftClient.getInstance().inGameHud.getChatHud().addMessage(Text.of("§c§lList of auto messages:"));
                     return 0;
                 })).then(ClientCommandManager.literal("add")
+                        .then(ClientCommandManager.argument("Id", StringArgumentType.string())
+                        .then(ClientCommandManager.argument("Delay", IntegerArgumentType.integer())
                         .then(ClientCommandManager.argument("Content", StringArgumentType.greedyString())
                                 .executes(context -> {
-
+                                    // json.put(id, [delay, content, enabled])
                                 }))
                         .executes(context -> {
-                    MinecraftClient.getInstance().inGameHud.getChatHud().addMessage(Text.of("it worked"));
+                    MinecraftClient.getInstance().inGameHud.getChatHud().addMessage(Text.of("/automessage add <id> <delay> <content>"));
                     return 0;
                 }))
         );
