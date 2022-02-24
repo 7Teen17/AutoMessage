f5dc9b293dfd63c9aa4be6cd6443414c0ae909fd
7Teen17 <7Teen17.email@gmail.com>
2022-02-23 21:07:56 -0800

    added CommandCreator.java and started the main command

--- src/main/java/com/github/seventeen/automessage/command/CommandCreator.java
+++ src/main/java/com/github/seventeen/automessage/command/CommandCreator.java
@@ -1,9 +1,18 @@
 package com.github.seventeen.automessage.command;
 
+import com.mojang.brigadier.arguments.StringArgumentType;
 import net.fabricmc.fabric.api.client.command.v1.ClientCommandManager;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.text.Text;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+// {int id: ["text string", int delayBetween, bool Enabled]}
 
 public class CommandCreator {
+    private Map<Integer, List> messages = new ArrayList<>();
 
     public static void createCommands() {
         ClientCommandManager.DISPATCHER.register(
@@ -13,7 +22,18 @@ public class CommandCreator {
                     }
                     MinecraftClient.getInstance().player.sendChatMessage("Testing123");
                     return 0;
-                })
+                }).then(ClientCommandManager.literal("list").executes(context -> {
+                    MinecraftClient.getInstance().inGameHud.getChatHud().addMessage(Text.of("§c§lList of auto messages:"));
+                    return 0;
+                })).then(ClientCommandManager.literal("add")
+                        .then(ClientCommandManager.argument("Content", StringArgumentType.greedyString())
+                                .executes(context -> {
+
+                                }))
+                        .executes(context -> {
+                    MinecraftClient.getInstance().inGameHud.getChatHud().addMessage(Text.of("it worked"));
+                    return 0;
+                }))
         );
     }
 }
